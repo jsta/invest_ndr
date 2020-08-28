b8c6e82f6f8a7931f341c850837af40a0d4b9046
jsta <stachel2@msu.edu>
2020-08-28 13:58:39 -0400

    towards understanding model internals

--- scripts/99_utils.R
+++ scripts/99_utils.R
@@ -1,4 +1,15 @@
-library(reticulate)
-use_condaenv(condaenv = "envs/invest")
+# load packages
+library(sf)
+library(raster)
+suppressMessages(library(dplyr))
+library(mapview)
 
-ndr <- import("natcap.invest.ndr.ndr")
+# set paths
+data_dir <- "data/NDR/"
+
+# utility functions
+raster_sum <- function(x) sum(x[], na.rm = TRUE)
+
+get_values_from_rasters <- function(sf_point, raster_list){
+  lapply(raster_list, function(x) raster::extract(x, sf_point))
+}
---
+++ scripts/expore_inputs.R
@@ -0,0 +1,81 @@
+source("scripts/99_utils.R")
+
+# ---- load data -----
+# user provided inputs
+input_tifs <- list(
+  raster(paste0(data_dir, "DEM_gura.tif")),
+  raster(paste0(data_dir, "land_use_gura.tif")),
+  raster(paste0(data_dir, "precipitation_gura.tif")))
+names(input_tifs) <- c("dem", "lulc", "precip")
+watersheds    <- st_read(paste0(data_dir,  "watershed_gura.shp"))
+biophys_table <- read.csv(paste0(data_dir,  "biophysical_table_gura.csv"))
+
+# derived inputs
+derived_dir   <- "workspace/intermediate_outputs/"
+derived_tifs  <- list.files(derived_dir, pattern = "*.tif",
+                           include.dirs = TRUE, full.names = TRUE)
+derived_names <- gsub(".tif", "", basename(derived_tifs))
+dt            <- lapply(derived_tifs, function(x) raster(x))
+names(dt)     <- derived_names
+dt            <- append(dt, input_tifs)
+
+# output
+output_dir <- "workspace"
+p_export   <- raster(paste0(output_dir, "/p_export.tif"))
+dt         <- append(dt, p_export)
+names(dt)[names(dt) == ""] <- "p_export"
+
+# ---- check model operation ----
+# generate test point
+set.seed(22)
+test_values <- dt$flow_accumulation[]
+test_index  <- sample(
+  which(!is.na(test_values) & test_values == 1), 1)
+test_xy     <- raster::xyFromCell(dt$flow_accumulation, test_index)
+
+sf_point <- st_as_sf(st_sfc(
+  st_point(test_xy),
+  crs = raster::projection(dt$flow_accumulation)
+  ))
+
+mapview(dt$flow_accumulation == 1, maxpixels = 1169217) +
+  mapview(sf_point)
+
+?whiches.min()
+
+# sf_point <- sf::st_point_on_surface(watersheds)
+# mapview(watersheds) + mapview(sf_point)
+dt_values <- get_values_from_rasters(sf_point, dt)
+
+# export is equal to load * delivery ratio
+all.equal(dt_values$p_export,
+          (dt_values$surface_load_p * dt_values$ndr_p) +
+            (dt_values$sub_load_p * dt_values$sub_ndr_p))
+
+# surface loading is a function of entries in the biophys_table
+(biophys_values <- dplyr::filter(biophys_table, lucode == dt_values$lulc))
+
+
+plot(dt$lulc)
+plot(dt$surface_load_p)
+
+dt_values$surface_load_p
+dt_values$load_p
+dt_values$lulc
+
+
+plot()
+
+
+
+
+
+
+
+
+
+
+
+
+
+mapview(watersheds)
